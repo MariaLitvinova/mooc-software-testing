a4d277a9c7dfa69a08fd250c83d1a04043f75637
MariaLitvinova <mariia.v.litvinova@gmail.com>
2019-11-03 21:37:43 +0300

    added tests for ghappy/GHappy.java

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,27 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void testExactlyOneOccurence() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xxggxx");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void testMoreThanOneOccurences() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("xxggxxgg");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void testShortestHappyString() {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("ggd");
+        Assertions.assertEquals(true, result);
+    }
 }
